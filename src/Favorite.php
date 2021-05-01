bdb6c8b744e7de1d75860fc70731dbb47076dbb8
Zing <zingimmick@outlook.com>
2021-05-01 23:21:32 +0800

    Simplify ternary if-else operator (#35)

--- src/Favorite.php
+++ src/Favorite.php
@@ -45,7 +45,11 @@ class Favorite extends MorphPivot
 
     public function getIncrementing(): bool
     {
-        return $this->uuids() ? true : parent::getIncrementing();
+        if ($this->uuids()) {
+            return true;
+        }
+
+        return parent::getIncrementing();
     }
 
     public function getKeyName(): string
