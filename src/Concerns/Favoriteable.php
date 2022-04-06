9c068867cc13eca0a707d6848351bb98eb00b234
Zing <zingimmick@outlook.com>
2022-04-06 20:16:14 +0800

    Add type hint for constants (#119)

--- src/Concerns/Favoriteable.php
+++ src/Concerns/Favoriteable.php
@@ -93,6 +93,8 @@ trait Favoriteable
     }
 
     /**
+     * @phpstan-param 1|2|3|4 $mode
+     *
      * @param array<int, string>|null $divisors
      */
     public function favoritersCountForHumans(
